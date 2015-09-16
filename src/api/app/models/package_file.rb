83aecfe6accff41dc60c11a975d5089712f85ffd
Moisés Déniz Alemán <mdeniz@suse.de>
2015-09-16 17:30:04 +0200

    [webui] Added the BackendFile class to manage files in the backend inside models

---
+++ src/api/app/models/package_file.rb
@@ -0,0 +1,12 @@
+class PackageFile < BackendFile
+
+  attr_accessor :project_name, :package_name
+
+  validates :project_name, :package_name, presence: true
+
+  def full_path(params = {})
+    query = params.blank? ? '' : "?#{params.to_query}"
+    URI::encode("/source/#{project_name}/#{package_name}/#{name}") + query
+  end
+
+end
